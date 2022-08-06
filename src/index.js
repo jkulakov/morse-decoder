71d710a57d4964946f984d97773dfbb1019c7e5f
Ivan Kulakov <ivankulakov@MacBook-Pro-Ivan.local>
2022-08-06 21:44:03 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,9 +38,39 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const symbols = [];
+    symbols[10] = '.';
+    symbols[11] = '-';
+    let words = expr.split('**********');
+
+    words.map((item, i, array) => {
+        let letterCount = item.length / 10;
+        let letters = [];
+        let start = 0,
+            end = 10;
+            for (let i = 0; i < letterCount; i++) {
+                letters[i] = item.slice(start, end);
+                start = start + 10; end = end + 10;
+                if(i == letterCount - 1) { start = 0; end = 10; }
+            }    
+        letters.map((item, i, arr) => {
+            let newLetter = '';
+            let jStart = 8;
+            let jEnd = 10;
+            for (let j = 5; j > 0; j--) {
+                if(item.slice(jStart, jEnd) != '00') newLetter = symbols[item.slice(jStart, jEnd)] + newLetter;
+                jStart -= 2;
+                jEnd -= 2;
+                if(j == 1) arr[i] = MORSE_TABLE[newLetter];  
+            }
+        });
+        array[i] = letters.join('');
+    });
+    return words.join(' ');
 }
 
 module.exports = {
     decode
 }
+
+decode('00101010100000000010001011101000101110100000111111**********00001011110000111111000010111000101110100000111010');
